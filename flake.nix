843f1a21d45d84d3de20ffce3653957e105b4194
Danie Roux <danie@danieroux.com>
2024-03-01 11:35:44 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -4,26 +4,53 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:danieroux/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        inherit (pkgs) python3;
         #  Need the unfree for terraform
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
       in
+      rec
       {
         formatter = pkgs.nixpkgs-fmt;
 
         packages = {
-          terravision = mkPoetryApplication {
-            projectDir = self;
-            packages = [ pkgs.poetry pkgs.graphviz pkgs.terraform ];
+
+          python-hcl2 = import ./python-hcl2.nix {
+          setuptools = python3.pkgs.setuptools;
+           setuptools-scm = python3.pkgs.setuptools-scm;
+            lib = python3.pkgs.lib;
+            buildPythonPackage = python3.pkgs.buildPythonPackage;
+            fetchPypi = python3.pkgs.fetchPypi;
+            lark = python3.pkgs.lark;
+            nose = python3.pkgs.nose;
+            pythonOlder = python3.pkgs.pythonOlder;
+          };
+
+          # https://discourse.nixos.org/t/pip-not-found-by-python39-in-nix-shell/24017/3
+          terravision = python3.pkgs.buildPythonPackage rec {
+            format = "pyproject";
+            name = "terravision";
+            src = ./.;
+            doCheck = false;
+            nativeBuildInputs = with python3.pkgs; [
+              setuptools
+            ];
+            propagatedBuildInputs = with python3.pkgs; [
+              clickclick
+              gitpython
+              graphviz
+              requests
+              tqdm
+              packages.python-hcl2
+              numpy
+              debugpy
+              ipaddr
+            ];
+
           };
           default = self.packages.${system}.terravision;
         };
